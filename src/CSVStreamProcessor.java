c937de539098f7d734790c20e4ae41f5e1df192e
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-05-07 20:17:14 +0200

    refactor : separate the methods for easy accessing

--- src/CSVStreamProcessor.java
+++ src/CSVStreamProcessor.java
@@ -8,15 +8,17 @@ import java.util.stream.Stream;
 public class CSVStreamProcessor {
     public static void calculateAverageAge(String csvFilePath) throws IOException {
         Path path = Path.of(csvFilePath);
-       try(Stream<String> line = Files.lines(path)){ ;
-        OptionalInt sum = line.skip(1)
-                .map(part -> part.split(","))
-                .filter(part -> part.length > 1 )
-                .map(part -> part[1].trim())
+       try(Stream<String> line = Files.lines(path)){
+           List<String> list= line.skip(1)
+                   .map(part -> part.split(","))
+                   .filter(part -> part.length > 1 )
+                   .map(part -> part[1].trim()).toList();
+
+        OptionalInt sum = list.stream()
                 .mapToInt(Integer::parseInt)
                 .reduce(Integer::sum);
 
-        System.out.println("the sum is : "+ (sum.isPresent() ? sum.getAsInt() : 0) );
+        double Average =  (double) (sum.isPresent() ? sum.getAsInt() : 0) /list.size() ;
        }
        catch (Exception e) {
            throw new RuntimeException(e);
