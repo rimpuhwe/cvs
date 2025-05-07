6a25d63ecc0dac27f5ef7c40303a47133913b413
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-05-07 20:13:06 +0200

    add a sum to test if it's get the number from file

--- src/CSVStreamProcessor.java
+++ src/CSVStreamProcessor.java
@@ -9,7 +9,15 @@ public class CSVStreamProcessor {
     public static void calculateAverageAge(String csvFilePath) throws IOException {
         Path path = Path.of(csvFilePath);
        try(Stream<String> line = Files.lines(path)){ ;
+        OptionalInt sum = line.skip(1)
+                .map(part -> part.split(","))
+                .filter(part -> part.length > 1 )
+                .map(part -> part[1].trim())
+                .mapToInt(Integer::parseInt)
+                .reduce(Integer::sum);
 
+        System.out.println("the sum is : "+ (sum.isPresent() ? sum.getAsInt() : 0) );
+       }
        catch (Exception e) {
            throw new RuntimeException(e);
        }
